fb5d011a71d7f3674aba26771604277ac718801c
مكتب ابو رحاب للتطوير العقاري <hmztzyadtarq@gmail.com>
2026-08-06 17:57:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-analytics.js";
